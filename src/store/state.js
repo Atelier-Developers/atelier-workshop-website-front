b05985ab67f4689517232cc49142274e902bdc29
amirbaghi <bamirmasoud@gmail.com>
2020-01-24 14:07:03 +0330

    fixed avatar

--- src/store/state.js
+++ src/store/state.js
@@ -1,7 +1,7 @@
 // https://vuex.vuejs.org/en/state.html
 
 export default {
-    api: 'http://37.152.180.226:5000',
+    api: 'http://localhost:5000',
     status: '',
     drawer: true,
     userId:  localStorage.getItem('userId') || -1,
